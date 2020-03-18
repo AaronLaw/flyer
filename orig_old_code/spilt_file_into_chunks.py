d729a18968c98486715761c37933c68e755d72c2
aaronlaw <aaronlaw@gmail.com>
2020-03-19 07:16:31 +0800

    Correct the code in 'spilt_file_into_chunks.py'.
    As a prototype of class SplitFileIntoChunks.

--- orig_old_code/spilt_file_into_chunks.py
+++ orig_old_code/spilt_file_into_chunks.py
@@ -1,28 +1,22 @@
-
-# with  open(filename) as fp:
-#     contents = fp.read()
-#     for entry in contents.split(':'):
-#         # do something with entry  
-#         print((entry))
-
-# with open("entry.txt") as f: 
-#     for line in f:
-#         if line[0] == "[":
-#             if out: out.close()
-#             out = open(line.split()[1] + ".txt", "w")
-#         else: 
-#             out.write(line)
+# The prototype of class SplitFileIntoChunks.
+#  Reference:
+# Google: python split file by delimiter -> Google: python split file into chunks by a given delimiter (=  Google: python split text file by delimiter)
+#    1. https://stackoverflow.com/questions/7980288/splitting-large-text-file-by-a-delimiter-in-python -> use [itertools.groupby](https://docs.python.org/3/library/itertools.html#itertools.groupby) to group lines after `----` into lists. This question is similar to mine (e.g. to split content in a text file into files by '----' as a seperator.)
 
 
+import itertools as it
 
-""" from Flyer import ReadFile, CreateFile
+filename = './sample_data/2020-03.md'
+# with open(filename, w) as file_obj:
+#     file_obj.write()
 
-def test_read_file(filename):
-    commands = [cmd for cmd in (ReadFile(filename), ReadFile(filename))]
-    [c.execute() for c in commands]
-    
-    content = ReadFile(filename).execute()
-    print(content)
+def get_list_of_content(filename, encoding='utf-8'):
+    """Open a file and return a list of contents.
+    """
+    with open(filename,'rt', encoding=encoding) as file_obj:
+        for key,group in it.groupby(file_obj, lambda line: line.startswith('----')):
+            if not key:
+                group = list(group) # convert group object into lists for futher processing.
+                print(group)
 
-# test_read_file(filename)
- """
+get_list_of_content(filename)
